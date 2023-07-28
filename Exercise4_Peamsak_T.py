cd0fd9b20643d19a57f9bf2bc45da0e7f867113a
Peamsak <125744875+Peamsak@users.noreply.github.com>
2023-07-28 12:49:32 +0700

    Update Exercise4_Peamsak_T.py

--- Exercise4_Peamsak_T.py
+++ Exercise4_Peamsak_T.py
@@ -1,1 +1,10 @@
-
+#เก็บคะแนน
+eng = 90.25
+bus = 100
+introcs = 79.5
+compro = 90.5
+print("--- Your Score ---")
+print(f"Foundation English : {eng}")
+print(f"General Business : {bus}")
+print(f"Introduction to Computer Systems : {introcs}")
+print(f"Computer Programming : {compro}")
